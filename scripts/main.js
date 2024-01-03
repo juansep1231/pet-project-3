11cf62fe7010fa43e00f1873c0c482cdb601330d
Juan Posso <juan.posso@epn.edu.ec>
2024-01-02 23:07:05 -0500

    feat: added functionality to get last 5 updated repos

--- scripts/main.js
+++ scripts/main.js
@@ -32,33 +32,68 @@ function getTopFiveRepos(){
 const apiUrl = "https://api.github.com/orgs/stackbuilders/repos";
 
 document.getElementById("getTopFive").addEventListener("click", getTopFiveRepos);
+document.getElementById("getLastUpdated").addEventListener("click", getLastUpdatedRepos);
 
+function getTopFiveRepos() {
 
-function getTopFiveRepos(){
+  fetch(apiUrl).then((response) =>
+    response.json()
+  ).then(data =>
+    Object.values(data)
+  ).then(data => data.filter(repo => repo.stargazers_count > 5))
+    .then(data => {
 
-    repos = fetch(apiUrl).then((response) => 
-        response.json()
-      ).then( data =>  
-        Object.values(data)
-      ).then( data => data.filter( repo => repo.stargazers_count > 5 ))
-      .then( data => {
+      let htmlOutput = '<h2> Repositories with more than 5 stars </h2>';
+      htmlOutput += '<table>';
+      htmlOutput += '<tr><th>Name</th><th>Stargazers Count</th></tr>';
 
-        let htmlOutput = '<h2> Repositories with more than 5 stars </h2>';
-        htmlOutput += '<table>';
-        htmlOutput += '<tr><th>Name</th><th>Stargazers Count</th></tr>';
-        
-        data.forEach(repo => {
-          htmlOutput += `
+      data.forEach(repo => {
+        htmlOutput += `
             <tr>
               <td>${repo.name}</td>
               <td>${repo.stargazers_count}</td>
             </tr>`;
-        });
-        
-        htmlOutput += '</table>'
-
-          document.getElementById("ouputRepos").innerHTML = htmlOutput;
       });
 
-    
+      htmlOutput += '</table>'
+
+      document.getElementById("ouputRepos").innerHTML = htmlOutput;
+    });
+
+
+}
+
+
+function getLastUpdatedRepos() {
+
+  fetch(apiUrl).then((response) =>
+    response.json()
+  ).then(data =>
+    Object.values(data)
+  ).then(data => data.sort((itemA, itemB) => {
+
+    const dateA = new Date(itemA.updated_at);
+    const dateB = new Date(itemB.updated_at);
+    return dateB - dateA;
+  }))
+    .then(data => data.slice(0, 5))
+    .then(data => {
+      let htmlOutput = '<h2> Last 5 updated repos </h2>';
+      htmlOutput += '<table>';
+      htmlOutput += '<tr><th>Name</th><th>Updated date</th></tr>';
+
+      data.forEach(repo => {
+        htmlOutput += `
+      <tr>
+        <td>${repo.name}</td>
+        <td>${repo.updated_at}</td>
+      </tr>`;
+      });
+
+      htmlOutput += '</table>'
+
+      document.getElementById("ouputRepos").innerHTML = htmlOutput;
+    });
+
+
 }
