216b7dea22d2961fdfde00870e5d890f07fc0da6
Juan Posso <juan.posso@epn.edu.ec>
2024-01-15 07:56:27 -0500

    "feat: Added test for empty, undefined, null and shorter data"

--- test/fixtures/completeData.js
+++ test/fixtures/completeData.js
@@ -34,21 +34,6 @@ const data = [
     stargazers_count: 4,
     updated_at: "2026-11-18T12:00:00Z",
   },
-  {
-    name: "repo8",
-    stargazers_count: 5,
-    updated_at: "2027-11-18T12:00:00Z",
-  },
-  {
-    name: "repo9",
-    stargazers_count: 6,
-    updated_at: "2028-11-18T12:00:00Z",
-  },
-  {
-    name: "repo10",
-    stargazers_count: 8,
-    updated_at: "2029-11-18T12:00:00Z",
-  },
 ];
 
 module.exports = { data: data };
--- test/unit/functionsAsyncAwait.test.js
+++ test/unit/functionsAsyncAwait.test.js
@@ -7,9 +7,14 @@ const {
   getLastUpdatedRepos,
 } = require("../../scripts/functionsAsyncAwait.js");
 
-const { data } = require("../fixtures/data.js");
+const { data: completeData } = require("../fixtures/completeData.js");
+const { data2 } = require("../fixtures/incompleteData.js");
 
-describe("getFilteredsStargazers", () => {
+beforeEach(() => {
+  data = [...completeData];
+});
+
+describe("getReposMoreThanFiveStars", () => {
   test("returns an array with repos with more than 5 stars", () => {
     expect(getFilteredsStargazers(data)).toEqual([
       {
@@ -27,46 +32,112 @@ describe("getFilteredsStargazers", () => {
         stargazers_count: 10,
         updated_at: "2025-11-18T12:00:00Z",
       },
-      {
-        name: "repo9",
-        stargazers_count: 6,
-        updated_at: "2028-11-18T12:00:00Z",
-      },
-      {
-        name: "repo10",
-        stargazers_count: 8,
-        updated_at: "2029-11-18T12:00:00Z",
-      },
     ]);
+  }),
+    test("returns an array with the name and the stars of repos with more than 5 stars", () => {
+      expect(getReposMoreThanFiveStars(data)).toEqual([
+        {
+          name: "repo1",
+          stars: 10,
+        },
+        {
+          name: "repo5",
+          stars: 9,
+        },
+        {
+          name: "repo6",
+          stars: 10,
+        },
+      ]);
+    });
+
+  //TEST FOR EMPTY, UNDEFINED AND NULL ARRAYS
+
+  describe("When an array is empty", () => {
+    test("returns an empty array", () => {
+      expect(getReposMoreThanFiveStars([])).toEqual([]);
+    });
   });
 
-  test("returns an array with the name and the stars of repos with more than 5 stars", () => {
-    expect(getReposMoreThanFiveStars(data)).toEqual([
-      {
-        name: "repo1",
-        stars: 10,
-      },
-      {
-        name: "repo5",
-        stars: 9,
-      },
-      {
-        name: "repo6",
-        stars: 10,
-      },
-      {
-        name: "repo9",
-        stars: 6,
-      },
-      {
-        name: "repo10",
-        stars: 8,
-      },
-    ]);
+  describe("When an array is undefined", () => {
+    test("returns an empty array", () => {
+      expect(getReposMoreThanFiveStars(undefined)).toEqual([]);
+    });
+  });
+
+  describe("When an array is null", () => {
+    test("returns an empty array", () => {
+      expect(getReposMoreThanFiveStars(null)).toEqual([]);
+    });
   });
 });
 
-describe("getLastUpdatedRepos", () => {
+describe(
+  "getLastUpdatedRepos",
+  () =>
+    test("returns an array with data sorted by date", () => {
+      expect(sortDataByDate(data)).toEqual([
+        {
+          name: "repo7",
+          stargazers_count: 4,
+          updated_at: "2026-11-18T12:00:00Z",
+        },
+        {
+          name: "repo6",
+          stargazers_count: 10,
+          updated_at: "2025-11-18T12:00:00Z",
+        },
+        {
+          name: "repo5",
+          stargazers_count: 9,
+          updated_at: "2024-11-18T12:00:00Z",
+        },
+        {
+          name: "repo4",
+          stargazers_count: 3,
+          updated_at: "2023-11-18T12:00:00Z",
+        },
+        {
+          name: "repo3",
+          stargazers_count: 1,
+          updated_at: "2022-11-18T12:00:00Z",
+        },
+        {
+          name: "repo2",
+          stargazers_count: 5,
+          updated_at: "2021-11-18T12:00:00Z",
+        },
+        {
+          name: "repo1",
+          stargazers_count: 10,
+          updated_at: "2020-11-18T12:00:00Z",
+        },
+      ]);
+    }),
+  test("returns an array with the name and the date of the latest updated repos", () => {
+    expect(getLastUpdatedRepos(data)).toEqual([
+      {
+        name: "repo7",
+        updated_date: "2026-11-18T12:00:00Z",
+      },
+      {
+        name: "repo6",
+        updated_date: "2025-11-18T12:00:00Z",
+      },
+      {
+        name: "repo5",
+        updated_date: "2024-11-18T12:00:00Z",
+      },
+      {
+        name: "repo4",
+        updated_date: "2023-11-18T12:00:00Z",
+      },
+      {
+        name: "repo3",
+        updated_date: "2022-11-18T12:00:00Z",
+      },
+    ]);
+  }),
   test("returns an array with the top five repos", () => {
     expect(getTopFive(data)).toEqual([
       {
@@ -95,25 +166,9 @@ describe("getLastUpdatedRepos", () => {
         updated_at: "2024-11-18T12:00:00Z",
       },
     ]);
-  });
-
-  test("returns an array with repos sorted in descending order based on the date", () => {
+  }),
+  test("returns an array with data sorted by date", () => {
     expect(sortDataByDate(data)).toEqual([
-      {
-        name: "repo10",
-        stargazers_count: 8,
-        updated_at: "2029-11-18T12:00:00Z",
-      },
-      {
-        name: "repo9",
-        stargazers_count: 6,
-        updated_at: "2028-11-18T12:00:00Z",
-      },
-      {
-        name: "repo8",
-        stargazers_count: 5,
-        updated_at: "2027-11-18T12:00:00Z",
-      },
       {
         name: "repo7",
         stargazers_count: 4,
@@ -150,36 +205,73 @@ describe("getLastUpdatedRepos", () => {
         updated_at: "2020-11-18T12:00:00Z",
       },
     ]);
-  });
+  }),
 
-  test("returns an array with the name and the date of the latest updated repos", () => {
-    expect(getLastUpdatedRepos(data)).toEqual([
-      {
-        name: "repo10",
-        updated_date: "2029-11-18T12:00:00Z",
-      },
-      {
-        name: "repo9",
-        updated_date: "2028-11-18T12:00:00Z",
-      },
-      {
-        name: "repo8",
-        updated_date: "2027-11-18T12:00:00Z",
-      },
-      {
-        name: "repo7",
-        updated_date: "2026-11-18T12:00:00Z",
-      },
-      {
-        name: "repo6",
-        updated_date: "2025-11-18T12:00:00Z",
-      },
-    ]);
-  });
-});
+  describe("When an array is filled with just 3 items", () => {
+    test("returns the complete array", () => {
+      expect(getTopFive(data2)).toEqual([
+        {
+          name: "repo1",
+          stargazers_count: 10,
+          updated_at: "2020-11-18T12:00:00Z",
+        },
+        {
+          name: "repo2",
+          stargazers_count: 5,
+          updated_at: "2021-11-18T12:00:00Z",
+        },
+        {
+          name: "repo3",
+          stargazers_count: 1,
+          updated_at: "2022-11-18T12:00:00Z",
+        },
+      ]);
+    });
+  }),
+
+  //TEST FOR EMPTY, UNDEFINED AND NULL ARRAYS
+
+  describe("When an array is empty", () => {
+    test("returns an empty array", () => {
+      expect(getLastUpdatedRepos([])).toEqual([]);
+    });
+  }),
+
+  describe("When an array is undefined", () => {
+    test("returns an empty array", () => {
+      expect(getLastUpdatedRepos(undefined)).toEqual([]);
+    });
+  }),
+
+  describe("When an array is null", () => {
+    test("returns an empty array", () => {
+      expect(getLastUpdatedRepos(null)).toEqual([]);
+    });
+  })
+);
 
 describe("getSumOfReposStars", () => {
-  test("returns the sum of all the repos stars", () => {
-    expect(getSumOfReposStars(data)).toEqual(61);
+  describe("When an array is valid", () => {
+    test("returns the sum of all the repos stars", () => {
+      expect(getSumOfReposStars(data)).toEqual(42);
+    });
+  });
+
+  describe("When an array is empty", () => {
+    test("returns an empty array", () => {
+      expect(getSumOfReposStars([])).toEqual([]);
+    });
+  });
+
+  describe("When an array is undefined", () => {
+    test("returns an empty array", () => {
+      expect(getSumOfReposStars(undefined)).toEqual([]);
+    });
+  });
+
+  describe("When an array is null", () => {
+    test("returns an empty array", () => {
+      expect(getSumOfReposStars(null)).toEqual([]);
+    });
   });
 });
