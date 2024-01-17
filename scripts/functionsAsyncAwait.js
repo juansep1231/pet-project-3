a687a3a47e50d2ffd9b54d0626896579f71a7863
Juan Posso <juan.posso@epn.edu.ec>
2024-01-17 08:38:28 -0500

    refactor: modifiying functions to use implicit return

--- scripts/functionsAsyncAwait.js
+++ scripts/functionsAsyncAwait.js
@@ -10,17 +10,13 @@ const getData = async () => {
 };
 
 //FUNCTION TO CHECK IF THE DATA IS EMPTY, NULL OR UNDEFINED
-const checkEmptyData = (dataArray) => {
-  return dataArray === undefined || dataArray === null || dataArray.length <= 0;
-};
+const checkEmptyData = (dataArray) =>
+  dataArray === undefined || dataArray === null || dataArray.length <= 0;
 
 //FUNCTION TO EXCEUTE A FUNCTION IF DATA IS NOT EMPTY, NULL OR UNDEFINED
-const checkEmptyDataAndExecuteFunction = (dataArray, executeFunction) => {
-  if (checkEmptyData(dataArray)) {
-    return [];
-  }
-  return executeFunction(dataArray);
-};
+
+const checkEmptyDataAndExecuteFunction = (dataArray, executeFunction) =>
+  checkEmptyData(dataArray) ? [] : executeFunction(dataArray);
 
 // FUCNTIONS TO RETURN THE REPOS WIHT MORE THAN FIVE STARS
 
@@ -29,50 +25,44 @@ const getArrayOfData = async () => {
   return Object.values(data);
 };
 
-const getFilteredsStargazers = (dataArray) => {
-  return checkEmptyDataAndExecuteFunction(dataArray, (data) =>
+const getFilteredsStargazers = (dataArray) =>
+  checkEmptyDataAndExecuteFunction(dataArray, (data) =>
     data.filter((repo) => repo.stargazers_count > 5)
   );
-};
 
-const getReposMoreThanFiveStars = (dataArray) => {
-  return checkEmptyDataAndExecuteFunction(dataArray, (data) =>
-    getFilteredsStargazers(data).map((repo) => {
-      return { name: repo.name, stars: repo.stargazers_count };
-    })
+const getReposMoreThanFiveStars = (dataArray) =>
+  checkEmptyDataAndExecuteFunction(dataArray, (data) =>
+    getFilteredsStargazers(data).map((repo) => ({
+      name: repo.name,
+      stars: repo.stargazers_count,
+    }))
   );
-};
 
 // FUCNTIONS TO RETURN THE 5 LAST UPDATED REPOS
 
-const getLastUpdatedRepos = (dataArray) => {
-  return checkEmptyDataAndExecuteFunction(dataArray, (data) => {
+const getLastUpdatedRepos = (dataArray) =>
+  checkEmptyDataAndExecuteFunction(dataArray, (data) => {
     const sortedData = sortDataByDate(data);
-    return getTopFive(sortedData).map((repo) => {
-      return { name: repo.name, updated_date: repo.updated_at };
-    });
+    return getTopFive(sortedData).map((repo) => ({
+      name: repo.name,
+      updated_date: repo.updated_at,
+    }));
   });
-};
 
-const getTopFive = (dataArray) => {
-  return checkEmptyDataAndExecuteFunction(dataArray, (data) =>
-    data.slice(0, 5)
+const getTopFive = (dataArray) =>
+  checkEmptyDataAndExecuteFunction(dataArray, (data) => data.slice(0, 5));
+
+const sortDataByDate = (dataArray) =>
+  checkEmptyDataAndExecuteFunction(dataArray, (data) =>
+    data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
   );
-};
-
-const sortDataByDate = (dataArray) => {
-  return checkEmptyDataAndExecuteFunction(dataArray, (data) => {
-    return data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
-  });
-};
 
 //FUNCTION TO GET THE SUM OF ALL REPOS STARS
 
-const getSumOfReposStars = (dataArray) => {
-  return checkEmptyDataAndExecuteFunction(dataArray, (data) =>
+const getSumOfReposStars = (dataArray) =>
+  checkEmptyDataAndExecuteFunction(dataArray, (data) =>
     data.reduce((acc, repo) => acc + repo.stargazers_count, 0)
   );
-};
 
 module.exports = {
   getReposMoreThanFiveStars: getReposMoreThanFiveStars,
